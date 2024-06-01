25b2225ad6b548a51854e99de2121363dc2f07e8
cristiankozlowski <cristiankozlowski@hotmail.com>
2024-06-01 12:12:55 -0300

    Documentation with scribe

--- app/Http/Controllers/Api/V1/TravelController.php
+++ app/Http/Controllers/Api/V1/TravelController.php
@@ -7,8 +7,20 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\TravelResource;
 
+/**
+ * @group Public endpoints
+ */
 class TravelController extends Controller
 {
+     /**
+     * GET Travels
+     *
+     * Returns paginated list of travels.
+     *
+     * @queryParam page integer Page number. Example: 1
+     *
+     * @response {"data":[{"id":"9958e389-5edf-48eb-8ecd-e058985cf3ce","name":"First travel", ...}}
+     */
     public function index()
     {
         $travels = Travel::where('is_public', true)->paginate();
